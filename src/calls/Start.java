1816cc8303285176941510352c02537c15aa8dab
Andrei-Predoiu <andrei.vn92@gmail.com>
2014-10-28 23:02:54 +0100

    updated start call

--- src/calls/Start.java
+++ src/calls/Start.java
@@ -22,68 +22,70 @@ import com.google.gson.GsonBuilder;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
+import server.model.ServerMessage;
 
 //done
-
 /**
  * Servlet implementation class OrderServlet
  */
-@WebServlet(urlPatterns = { "/start" })
+@WebServlet(urlPatterns = {"/start"})
 public class Start extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	Gson gson = new GsonBuilder().create();
 
-	private Configuration cfg = FreemarkerConfig.getInstance();
-	DataManipulator worker = DataManipulator.getInstance();
+   private static final long serialVersionUID = 1L;
+   Gson gson = new GsonBuilder().create();
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public Start() {
-		super();
-	}
+   private Configuration cfg = FreemarkerConfig.getInstance();
+   DataManipulator worker = DataManipulator.getInstance();
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+   /**
+    * @see HttpServlet#HttpServlet()
+    */
+   public Start() {
+      super();
+   }
 
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-	}
+   /**
+    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+    * response)
+    */
+   protected void doGet(HttpServletRequest request,
+           HttpServletResponse response) throws ServletException, IOException {
+   }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+   /**
+    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+    * response)
+    */
+   protected void doPost(HttpServletRequest request,
+           HttpServletResponse response) throws ServletException, IOException {
+      response.setContentType("text;charset=UTF-8");
+      PrintWriter writer = response.getWriter();
+      Map<String, String> root = new HashMap<String, String>();
+      root.put("message", "failed");
+      try {
 
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text;charset=UTF-8");
-		PrintWriter writer = response.getWriter();
-		Map<String, String> root = new HashMap<String, String>();
-		root.put("message", "failed");
-		try {
+         ServletContext sc = this.getServletContext();
+         String r = request.getParameter("message");
+         System.out.println(r);
 
-			ServletContext sc = this.getServletContext();
-			String r = request.getParameter("message");
-			System.out.println(r);
-			if (r.equals("{\"start\":\"true\"}")) {
-				root.put("message", worker.start());
-			}
-			/* Get the template */
-			Template temp = cfg.getTemplate("response.ftl");
+         ServerMessage req = gson.fromJson(r, ServerMessage.class);
 
-			/* Merge data-model with template */
-			try {
-				temp.process(root, writer);
-			} catch (TemplateException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} finally {
-			writer.close();
-		}
+         if (req.getResult().equals("start")) {
+            root.put("message", worker.start());
+         }
+         /* Get the template */
+         Template temp = cfg.getTemplate("response.ftl");
 
-	}
+         /* Merge data-model with template */
+         try {
+            temp.process(root, writer);
+         } catch (TemplateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+         }
+      } finally {
+         writer.close();
+      }
+
+   }
 }
---
+++ src/server/model/ServerMessage.java
@@ -0,0 +1,14 @@
+package server.model;
+
+public class ServerMessage {
+
+	private String result;
+
+	public ServerMessage(String result) {
+		this.result = result;
+	}
+
+	public String getResult() {
+		return result;
+	}
+}
